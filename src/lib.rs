3cf117cf5484f5c3ea801450b8c1d13d86201603
Austin Rhodes <warhodes@austins-mbp-2.lan>
2022-02-18 11:03:57 -0500

    Finalized version before adding pipes/redirects

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,13 @@ use std::io::{stdin, stdout, Write};
 use std::process::Command;
 use std::error;
 use std::env;
-use std::path;
 use std::process;
 
 pub struct Configuration {
     pub prompt: String
 }
 
-pub fn run(config: &Configuration) -> Result<(), Box<dyn error::Error>>{
+pub fn run(_config: &Configuration) -> Result<(), Box<dyn error::Error>>{
     loop {
         
         /* === Print Prompt === */
@@ -17,47 +16,48 @@ pub fn run(config: &Configuration) -> Result<(), Box<dyn error::Error>>{
         let home_dir    = env::var("HOME")?;
         let user        = env::var("USER")?;
         let current_dir = env::current_dir()?;
-        if let Ok(prompt_path) = current_dir.strip_prefix(home_dir) {
+        if let Ok(prompt_path) = current_dir.strip_prefix(&home_dir) {
             print!("{} ~/{}> ", user, prompt_path.display());
         } else {
-            print!("{}:{}>", config.prompt, current_dir.display());
+            print!("{} {}> ", user, current_dir.display());
         }
         stdout().flush()?;
 
-        /* === Read Input Line === */
+        /* === Read and Parse Input Line === */
         let mut input = String::new();
         stdin().read_line(&mut input)?;
         let fields_vec: Vec<&str> = input.trim().split_whitespace().collect();
-        let mut fields = fields_vec.iter();
+        let mut fields = fields_vec.into_iter();
         let command = match fields.next() {
             Some(c) => c,
             None => continue,
         };
-
         let mut args = fields;
 
-//      println!("command: {:?}", command);
-//      println!("args: {:?}", args);
-//
-        /* === Built-ins === */
 
-        match *command {
+        /* === Execute Command === */
+        match command {
             "exit" => {
                 println!("Thanks for using ash!");
-                process::exit(0);
+                return Ok(());
             },
             "cd" => {
-                env::set_current_dir(args.next().unwrap())?;
-                continue;
+                match args.next() {
+                    Some(c) => { env::set_current_dir(c)?; }
+                    None => { env::set_current_dir(home_dir)?; }
+                }
             },
-            _ => ()
+            command => {
+                let child = Command::new(command)
+                    .args(args)
+                    .spawn();
+
+                match child {
+                    Ok(mut child) => { child.wait()?; },
+                    Err(e) => { println!("{}", e); },
+                }
+            }
         }
 
-        /* === Fork and Exec === */
-        let mut child = Command::new(command)
-            .args(args)
-            .spawn()?;
-
-        child.wait()?;
     }
 }
